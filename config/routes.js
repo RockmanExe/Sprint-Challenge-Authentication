950c797d534218f88d0221abfb4b71e2b59c4fa5
unknown <may.alex43@gmail.com>
2018-08-17 12:57:58 -0400

    Adalberto May- wrote register and login functions

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
+const bcrypt= require('bcryptjs');
+const jwt= require('jsonwebtoken');
+const db= require('../database/dbConfig');
+const secret=require('../_secrets/keys');
 
 const { authenticate } = require('./middlewares');
 
@@ -10,11 +14,55 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  const user=req.body;
+  const hash=bcrypt.hashSync(user.password, 10);
+  user.password=hash;
+
+  db('users')
+  .insert(user)
+  .then(function(ids){
+    db('users')
+    .where({id:ids[0]})
+    .first()
+    .then(user=>{
+      const token=generateToken(user);
+      res.status(200).json({token})
+    });
+  })
+  .catch(error => {
+    res.status(500).json(error)
+  })
 }
+
+function generateToken(user){
+  const payload={
+    username: user.username,
+  };
+  const options={
+    expiresIn:'2h',
+    jwtid:'jkl;'
+  };
+  return jwt.sign(payload, secret, options);
+};
 
 function login(req, res) {
   // implement user login
-}
+  const credentials=req.body;
+  db('users')
+  .where({username:credentials.username})
+  .first()
+  .then(function (user){
+    if (user && bcrypt.compareSync(credentials.password, username.password)){
+      const token =generateToken(user);
+      res.send(`${user.username} signed in`);
+    } else {
+      return res.status(401).json({error:'Username or password incorrect'});
+    }
+  })
+  .catch(function(error){
+    res.status(500).json({error});
+  });
+};
 
 function getJokes(req, res) {
   axios
